3c915a24fc3f8006b42f439bb3a7716c96e4aa92
Johan <johan.e.lundin@hotmail.com>
2019-11-13 20:09:04 +0100

    First commit of Tolkien. Using NLP to extract commands from text

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
 from parlay import Parlay
+from tolkien import Tolkien
+
+sent = [
+		"Hei",
+		"Kjør frem i 5 sekunder",
+		"Kan du kjøre frem?",
+		"Jeg vil at du skal kjøre frem i 5 sekunder",
+		"Jeg vil at du skal kjøre frem i 5 minutter",
+		"Kjør frem i 5 sekunder. Stopp etterpå.",
+		"Kjør fram i 5 sekunder",
+		"Kan du kjøre fram?",
+		"Jeg vil at du skal kjøre fram i 5 sekunder",
+		"Jeg vil at du skal kjøre fram i 5 minutter",
+		"Kjør fram i 5 sekunder. Stopp etterpå.",
+		# "Kjør fremover i 5 sekunder",
+		"Kjør baklengs i 5 sekunder",
+		"Kan du kjøre baklengs?",
+		"Jeg vil at du skal kjøre baklengs i 5 sekunder",
+		"Jeg vil at du skal kjøre baklengs i 5 minutter",
+		"Kjør baklengs i 5 sekunder. Stopp etterpå.",
+		"Stopp nå",
+		"Snu til høyre",
+		"Snu til venstre",
+		"Snu mot høyre",
+		"Snu mot venstre",
+		"Snu deg 5 grader til høyre",
+		"Snu deg 5 grader til venstre",
+		"Kjør frem i 10 sekunder. Etterpå skal du stoppe."
+	]
 
 def main():
+	tolk = Tolkien()
+	# while True:
+	# 	inp = input("Tekst: ")
+	# 	tolk.pos_info_dump(inp)
+		# tolk.extract_commands(inp)
+		# tolk.print_commands()
+		# print(tolk.certainty_of_commands_parsing)
+
 	w = Parlay()
 	w.calibrate_recognizer()
+	print("POWER ON!")
 	while True:
-		[ret_code, ret_data] = w.listen(timeout = 0.5, time_limit = 3)
+		[ret_code, ret_data] = w.listen(time_limit = 3, timeout = 0.5)
 		if ret_code == 1:
 			speech_text = w.recognize(ret_data)
+			tolk.extract_commands(speech_text)
 			print(speech_text)
-		elif ret_code == 0:
-			print(ret_code)
+			tolk.print_commands()
+		# elif ret_code == 0:
+		# 	print(ret_code)
+
+
 main()
---
+++ tolkien.py
@@ -0,0 +1,70 @@
+import spacy
+from spacy import displacy
+from spacy.language import Language
+from spacy.lang.nb import Norwegian
+from spacy.lemmatizer import Lemmatizer
+
+class Tolkien:
+	def __init__(self, lang = 'no'):
+		self.nlp = spacy.load('nb_core_news_sm')
+		self.commands_in_text = []
+		self.commands_structure = commands.get('no')
+		self.certainty_of_commands_parsing = 1
+
+	def set_lang(self, lang):
+		if lang == 'no':
+			self.nlp = spacy.load(commands.get('no'))
+		elif lang == 'en':
+			self.nlp = spacy.load(commands.get('en'))
+
+	def extract_commands(self, sentence):
+		self.commands_in_text = []
+		sent = self.nlp(sentence)
+		number_of_commands = 0
+		number_of_correct_lemmas = 0
+		for token in sent:
+			if token.pos_ == "NUM":
+				self.commands_in_text.append(token.text)
+				continue
+			for com in self.commands_structure:
+				if token.lemma_.capitalize() == com:
+					number_of_commands += 1
+					self.commands_in_text.append(com)
+					if token.pos_ == self.commands_structure.get(com)[0]:
+						number_of_correct_lemmas += 1
+		# self.certainty_of_commands_parsing = number_of_correct_lemmas / number_of_commands
+
+	def pos_info_dump(self, sentence):
+		sent = self.nlp(sentence)
+		for token in sent:
+			print(token.text, token.lemma_, token.pos_, token.dep_, token.is_alpha, token.is_stop)
+
+	def print_commands(self):
+		print(self.commands_in_text)
+
+	def stringify_commands(self):
+		string_of_commands = ""
+		for com in self.commands_in_text:
+			string_of_commands += (com + " ")
+		return string_of_commands
+
+
+
+commands = {
+	'no' : {
+		"Hei" : [ "INTJ", 0],
+		"Kjøre" : [ "VERB", 0 ],
+		"Stoppe" : [ "VERB", 0],
+		"Snu" : [ "VERB", 0],
+		"Frem" : [ "ADP", 1 ],
+		"Fram" : [ "ADP", 1 ],
+		"Fremover" : [ "ADP", 1 ], # NOT CURRENTLY WORKING
+		"Baklengs" : [ "ADV", 1 ],
+		"Høyre" : [ "NOUN", 2],
+		"Venstre" : [ "NOUN", 2],
+		"Minutt" : [ "NOUN", 3],
+		"Sekund" : [ "NOUN", 3],
+		"Millisekund" : [ "NOUN", 3],
+		"Grad" : [ "NOUN", 3]
+	}
+}
